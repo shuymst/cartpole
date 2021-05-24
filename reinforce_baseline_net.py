919b306a14dc7f05ef50a764ed80e45c17dab71a
Shuhei Yamashita <yamashita-shuhei@g.ecc.u-tokyo.ac.jp>
2021-05-24 22:45:19 +0900

    no message

--- reinforce_baseline_net.py
+++ reinforce_baseline_net.py
@@ -2,15 +2,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 import gym
 import torch
-import os
 from torch import nn
 import network
 from network import PolicyNetwork, ValueNetwork
 
 def train(episode=1000, learning_rate=1e-3):
     env = gym.make("CartPole-v1")
-    model = PolicyNetwork(env)
-    optimizer = torch.optim.Adam(model.parameters(), lr = learning_rate)
+    policy_model = PolicyNetwork(env)
+    value_model = ValueNetwork(env)
+    policy_optimizer = torch.optim.Adam(policy_model.parameters(), lr = learning_rate)
+    value_optimizer = torch.optim.Adam(value_model.parameters(), lr = learning_rate)
     total_rewards = []
     for iter in range(episode):
         states = []
@@ -19,8 +20,9 @@ def train(episode=1000, learning_rate=1e-3):
         state = env.reset()
         states.append(state)
         total_reward = 0
-        for step in range(500):
-            action_probs = model(state).detach().numpy()
+
+        for step in range(500): # エピソード生成
+            action_probs = policy_model(state).detach().numpy()
             action = np.random.choice([0, 1], p = action_probs)
             next_state, reward, done, _ = env.step(action)
             actions.append(action)
@@ -32,24 +34,31 @@ def train(episode=1000, learning_rate=1e-3):
             state = next_state
         
         total_reward_tmp = total_reward
-        loss = torch.zeros(1, dtype = torch.float32)
-        for i, (s, a) in enumerate(zip(states, actions)):
+        policy_loss = torch.zeros(1, dtype = torch.float32)
+        value_loss = torch.zeros(1, dtype = torch.float32)
+        for i, (s, a) in enumerate(zip(states, actions)): # パラメータ更新
             g = total_reward_tmp
-            loss += -g * torch.log(model(s)[a]) # model(s)[a] = pi(a|s)
+            delta = g - value_model(s)
+            value_loss += delta * delta
+            policy_loss += -delta.detach() * torch.log(policy_model(s)[a]) # model(s)[a] = pi(a|s)
             total_reward_tmp -= rewards[i]
-        loss /= len(states)
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+        #loss /= len(states)
+        value_loss /= len(states)
+        policy_loss /= len(states)
+
+        value_optimizer.zero_grad()
+        value_loss.backward()
+        value_optimizer.step()
+
+        policy_optimizer.zero_grad()
+        policy_loss.backward()
+        policy_optimizer.step()
         total_rewards.append(total_reward)
         if iter % 100 == 0:
             print(f"episode:{iter} reward:{total_reward}")
     fig = plt.figure()
     ax = fig.add_subplot(111, xlabel = "episode", ylabel='total rewards')
-    ax.set_title("update by episode")
     ax.plot(range(1, episode + 1), total_rewards)
-    save_dir = "./figure/"
-    plt.savefig(os.path.join(save_dir, 'update_by_episode'))
     plt.show()
 
 if __name__ == "__main__":
